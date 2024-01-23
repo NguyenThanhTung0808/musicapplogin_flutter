b28b57a7a52dea720d09840139a7cd2a12186ba1
Tung <tungsoikl01@gmail.com>
2024-01-23 18:40:11 +0700

    musicandlogin

---
+++ android/app/src/main/kotlin/com/example/musicapplogin_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.musicapplogin_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model/musicapplogin_page.dart
@@ -0,0 +1,238 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MusicapploginPage extends StatefulWidget {
+  const MusicapploginPage({super.key});
+
+  @override
+  State<MusicapploginPage> createState() => _MusicapploginPageState();
+}
+
+class _MusicapploginPageState extends State<MusicapploginPage> {
+  String email = "";
+  String senha = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment(0.8, 1),
+          colors: <Color>[
+            Color(0xff70e7c1),
+            Color(0xff84e8c2),
+            Color(0xffe7efee),
+            Color(0xffe7efed),
+            Color(0xffe7f1ee),
+            Color(0xfff2f6f5),
+            Color(0xffe6eeeb),
+            Color(0xffedf5f1),
+          ],
+          tileMode: TileMode.mirror,
+        ),
+      ),
+      child: Center(
+          child: SingleChildScrollView(
+              child: Form(
+                  child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(height: 55),
+                            Image.asset("assets/images/img_logo.png"),
+                            SizedBox(height: 55),
+                            const Text(
+                              "Acesse sua conta",
+                              style: TextStyle(fontWeight: FontWeight.w700),
+                            ),
+                            SizedBox(height: 10),
+                            Padding(
+                              padding: EdgeInsets.only(left: 2),
+                              child: TextField(
+                                onChanged: (newEmail) {
+                                  print(newEmail);
+                                  email = newEmail;
+                                },
+                                keyboardType: TextInputType.text,
+                                style: TextStyle(fontSize: 10),
+                                decoration: InputDecoration(
+                                    labelText: 'E-mail',
+                                    border: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10))),
+                              ),
+                            ),
+                            SizedBox(height: 20),
+                            Padding(
+                              padding: EdgeInsets.only(left: 2),
+                              child: TextField(
+                                onChanged: (newSenha) {
+                                  print(newSenha);
+                                  senha = newSenha;
+                                },
+                                keyboardType: TextInputType.text,
+                                style: TextStyle(fontSize: 10),
+                                decoration: InputDecoration(
+                                    labelText: 'Senha',
+                                    border: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10))),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.only(left: 210),
+                              child: Text.rich(TextSpan(
+                                  text: 'Esqueci minha senha',
+                                  style: TextStyle(
+                                    fontSize: 13,
+                                    decoration: TextDecoration.underline,
+                                  ))),
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Padding(
+                                padding: EdgeInsets.only(left: 2),
+                                child: Container(
+                                  width: double.infinity,
+                                  height: 60,
+                                  alignment: Alignment.center,
+                                  decoration: const BoxDecoration(
+                                      color: Colors.green,
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(10))),
+                                  child: const Text('Acessar',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.w700,
+                                      )),
+                                )),
+                            SizedBox(height: 5),
+                            Padding(
+                                padding: EdgeInsets.only(left: 2),
+                                child: Container(
+                                  width: double.infinity,
+                                  height: 60,
+                                  alignment: Alignment.center,
+                                  decoration: const BoxDecoration(
+                                      color: Colors.black12,
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(10))),
+                                  child: const Text('Não tem uma conta? Crie agora',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 10,
+                                        fontWeight: FontWeight.w700,
+                                      )),
+                                )),
+                            const SizedBox(
+                              height: 50,
+                            ),
+                            Text("ou"),
+                            Stack(
+                              children: [
+                                Positioned(
+                                  child: Image.asset(
+                                    "assets/images/img_google.png",
+                                    width: 60,
+                                    height: 55,
+                                  ),
+                                ),
+                                Padding(
+                                    padding: EdgeInsets.only(left: 2),
+                                    child: Container(
+                                      width: double.infinity,
+                                      height: 60,
+                                      alignment: Alignment.center,
+                                      decoration: const BoxDecoration(
+                                          color: Colors.black12,
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(15))),
+                                      child: const Text('Acessar com o Gmail',
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 10,
+                                            fontWeight: FontWeight.w700,
+                                          )),
+                                    )),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Stack(
+                              children: [
+                                Positioned(
+                                  child: Image.asset(
+                                    "assets/images/img_apple.png",
+                                    width: 60,
+                                    height: 55,
+                                  ),
+                                ),
+                                Padding(
+                                    padding: EdgeInsets.only(left: 2),
+                                    child: Container(
+                                      width: double.infinity,
+                                      height: 60,
+                                      alignment: Alignment.center,
+                                      decoration: const BoxDecoration(
+                                          color: Colors.black12,
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(15))),
+                                      child: const Text('Acessar com o Apple ID',
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 10,
+                                            fontWeight: FontWeight.w700,
+                                          )),
+                                    )),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Stack(
+                              children: [
+                                Positioned(
+                                  child: Image.asset(
+                                    "assets/images/img_fb.png",
+                                    width: 60,
+                                    height: 55,
+                                  ),
+                                ),
+                                Padding(
+                                    padding: EdgeInsets.only(left: 2),
+                                    child: Container(
+                                      width: double.infinity,
+                                      height: 60,
+                                      alignment: Alignment.center,
+                                      decoration: const BoxDecoration(
+                                          color: Colors.black12,
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(15))),
+                                      child: const Text('Acessar com o Facebook',
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 10,
+                                            fontWeight: FontWeight.w700,
+                                          )),
+                                    )),
+                              ],
+                            ),
+                          ]))))),
+    ));
+  }
+}
