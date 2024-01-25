5c68244b0a0c0aadc03df01d55c3a1867c5ef5da
Tung <tungsoikl01@gmail.com>
2024-01-25 15:01:10 +0700

    music app login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:musicapplogin_flutter/page/musicapplogin_page.dart';
 
-import 'model/musicapplogin_page.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MusicapploginPage(),
+      home: const MusicAppLoginPage(),
     );
   }
 }
